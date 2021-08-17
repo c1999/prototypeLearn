0664cd25f1641ec958c50036fb5cdc8c98bc8ea8
zhujiawie <798474927@qq.com>
2021-08-17 11:51:52 +0800

    提交

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function Test() {}
 const test = new Test()
 // test.constructor -->实例化test对象的构造函数
-console.log('test.constructor === Test :>> ', test.constructor === Test) // true
+console.log('test.constructor === Test :>> ', test.constructor === Test) // true1
