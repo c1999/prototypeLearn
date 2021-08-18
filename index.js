b392b87c6bd45e86daabeb590c39ed3878bfd974
zhujiawie <798474927@qq.com>
2021-08-18 14:31:29 +0800

    门禁

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function Test() {}
-const test = new Test()
-// test.constructor -->实例化test对象的构造函数
-console.log('test.constructor === Test :>> ', test.constructor === Test) // true1
+const a = [1,3]
+const b = [ { id: 1,name: '1'},{ id: 2, name: '2'}]
+const c = b.filter((item)=>{
+    return a.indexOf(item.id) !== -1
+})
+console.log('c :>> ', c);
